0b9b6d01a1006b24dce8c5fe0435d7746b0b95a7
rohit <rohit.khot@sportzinteractive.net>
2022-10-27 11:15:27 +0530

    add

---
+++ ECN/ECN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ECN
+//
+//  Created by Poras Malviya on 20/01/20.
+//  Copyright © 2020 Sportz Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# swift
+# wm-ecn-ios
+
+European Cricket Network iOS App
+
+Code review checklist: 
+https://docs.google.com/spreadsheets/d/1aua4mUQQsQe2i8B1C6wDl3MA4LUEBvCpbzBufvP7TZA/edit#gid=844928795
